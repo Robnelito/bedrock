844c96f7739b66cdd1ffdad8c3b3abff0b905844
Robnelito <christianorak@gmail.com>
2021-05-11 18:00:35 +0300

    make some header

--- web/app/themes/understrap/inc/enqueue.php
+++ web/app/themes/understrap/inc/enqueue.php
@@ -19,8 +19,8 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
-
 		wp_enqueue_style('style-footer', get_template_directory_uri() . '/style.css', array(), $css_version);
+		wp_enqueue_style('style-header', get_template_directory_uri() . '/css/header.css', array(), $css_version);
 
 		wp_enqueue_script( 'jquery' );
 
@@ -28,7 +28,8 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
 
 		wp_enqueue_script('ow', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true);
-
+		wp_enqueue_script('ow', get_template_directory_uri() . '/Asset/asset/dist/owl.carousel.min.js', array(), $js_version, true);
+		
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
