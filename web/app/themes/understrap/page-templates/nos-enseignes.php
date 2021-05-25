fbb1b57a4632bc7ab965219ebe50bce3da2253b4
Robnelito <christianorak@gmail.com>
2021-05-25 14:35:19 +0300

    first commit in this branch\

--- web/app/themes/understrap/page-templates/nos-enseignes.php
+++ web/app/themes/understrap/page-templates/nos-enseignes.php
@@ -15,20 +15,27 @@ get_header();
 ?>
 
 <div id="content">
+  <div class="container" id="event-area">
+    
+  </div>
+</div>
 
 <?php
-$args = array('post_type' => 'nos_enseignes', 'order' => 'ASC', 'posts_per_page' => -1);
-$wp_query = new WP_Query($args);
+  $args = array('post_type' => 'nos_enseignes', 'order' => 'ASC', 'posts_per_page' => -1);
+  $wp_query = new WP_Query($args);
 ?>
 
-<div class="row">
+<div id="content">
+  <div class="">
+    <?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
+    <p><?php echo the_field("enseigne");?></p>
+  </div>
 
-  <?php while()?>
-
+  <?php
+    endwhile;
+    wp_reset_postdata();
+  ?>
 </div>
-
-</div>
-
 <?php
 get_footer();
 ?>
